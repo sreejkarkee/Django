687e76ab3f04dacad250d2834a8dbacad3e3e705
SriJan <srijan1769@gmail.com>
2026-09-20 10:51:51 +0545

    ..

--- core/home/models.py
+++ core/home/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    id = models.AutoField()
     name = models.CharField(max_length=100)
     age = models.IntegerField(default=18)
     email = models.EmailField()
